93dc93cbff094422345940232775c15436129496
Dmitriy Mozgovoy <robotshara@gmail.com>
2020-03-26 15:22:05 +0200

    updated README.md;

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -1,16 +1,22 @@
-const {type, string, number} = require("../src/define-accessor2");
+const {type, string, number, array} = require("../src/define-accessor2");
 
 class Cat {
     @string
     name = '';
-    @type(string|number)
-    foo= 123;
+    @type(string | number)
+    foo = 123;
+    @array
+    bar = [];
 }
 
 const cat = new Cat();
 cat.name = 'Lucky'; // Ok
-cat.foo= 123;
-cat.foo= '123';
+cat.foo = 123;
+cat.foo = '123';
+cat.bar = [1, 2, 3];
 
-cat.foo= true; // TypeError: Property foo accepts String|Number, but boolean given
 cat.name = 123; // TypeError: Property name accepts String, but number given
+cat.foo = true; // TypeError: Property foo accepts String|Number, but boolean given
+cat.bar= {}; //Property bar accepts Array, but object given
+
+
